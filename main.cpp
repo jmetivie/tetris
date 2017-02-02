c7197c66dea206b9756ff444f640e8bac1bf37f2
Metivier Jean-Philippe <metivier@ptilutin.info.unicaen.fr>
2017-02-02 17:07:57 +0100

    Looping using keyboard rather than a cin

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+
 #include "board.hpp"
 #include "tetramino.hpp"
 
@@ -13,58 +15,57 @@ int main() {
 	Tetramino t = Tetramino::randomTetraminoFactory();
 	board.addPiece(t, make_pair((board.getWidth()/2-1),0));
 	
-	while(true) {
-		board.display();
-		cout << "R: ROTATE | G,D,B:TRANSLATE | F:FALL" << endl;
-		char pressed = ' ';
-		cin >> pressed;
-		bool res = false;
-		switch (pressed) {
-			case 'R':
-				res = t.doRotate(board);
-				break;
-			case 'G':
-				res = t.doTranslate(board, "left");
-				break;
-			case 'D':
-				res = t.doTranslate(board, "right");
-				break;
-			case 'B':
-				res = t.doTranslate(board, "down");
-				break;
-			case 'F': {
-				res = t.doFall(board);
-				list<unsigned int> lines = board.getCompleteLines();
-				cout << "Complete: ";for (unsigned int l: lines) cout << l << " "; cout << endl;
-				board.removeLines(lines);
-				t = Tetramino::randomTetraminoFactory();
-				board.addPiece(t, make_pair((board.getWidth()/2-1),0));
-				break;
-			}
-			default : 
-				cout << "oupsy" << endl;
-		}
-		if (!res) cout << "no move was down" << endl;
+	//HERE LOOP
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		return 1;
 	}
+	SDL_Window* gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN );
 	
-	
-	
-	/*
-	OPiece o;
-	board.addPiece(o,make_pair(3,5));
-	TPiece t;
-	board.addPiece(t,make_pair(5,3));
-	board.display();
-	
-	cout << "canRotate?" << t.canRotate(board) << endl;
-	t.doRotate(board);
-	board.display();
-	
-	cout << "canTranslate?" << t.canTranslate(board,"left") << endl;
-	t.doTranslate(board,"left");
-	board.display();
-	*/
-	
+	bool quit = false;
+	SDL_Event e;
+	while( !quit )  {
+		 while( SDL_PollEvent( &e ) != 0 ) {
+			board.display();
+			cout << "R: UP ARROW | ARROWS:TRANSLATE | SPACE:FALL" << endl;
+			if( e.type == SDL_QUIT ) quit = true;
+			if( e.type == SDL_KEYDOWN ) {
+				bool res = false;
+				switch( e.key.keysym.sym ) {
+					case SDLK_UP: {
+						res = t.doRotate(board);
+						break;
+					}
+					case SDLK_LEFT: {
+						res = t.doTranslate(board, "left");
+						break;
+					}
+					case SDLK_RIGHT: {
+						res = t.doTranslate(board, "right");
+						break;
+					}
+					case SDLK_DOWN: {
+						res = t.doTranslate(board, "down");
+						break;
+					}
+					case SDLK_SPACE: {
+						res = t.doFall(board);
+						list<unsigned int> lines = board.getCompleteLines();
+						cout << "Complete: ";for (unsigned int l: lines) cout << l << " "; cout << endl;
+						board.removeLines(lines);
+						t = Tetramino::randomTetraminoFactory();
+						board.addPiece(t, make_pair((board.getWidth()/2-1),0));
+						break;
+					}
+					default:
+						cout << "WHAT !!! " << endl;
+						break;
+				 }
+			 }
+		 }
+	}
+
 	return 0;
 }
 
